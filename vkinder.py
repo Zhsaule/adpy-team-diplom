a3ffc431d5cdd475f82b99fd533bbf9e666b49b2
Валеев Ильяс <heymaker279@gmail.com>
2022-05-13 20:38:16 +0500

    add_fix

--- vkinder.py
+++ vkinder.py
@@ -1,63 +1,63 @@
 import random
+from pprint import pprint
 import requests
 from Token import GROUP_TOKEN, personal_token  # персональный токен
-
 access_token = personal_token
 
-
+coincidence = []
 class VKinder_get_info:
-
     def __init__(self, sex, age, city):
-        self.sex = sex
-        self.age = age
-        self.city = city.title()
         if sex == 'ж':
-            sex_ = 2
+            sex = 2
         elif sex == 'м':
-            sex_ = 1
+            sex = 1
         else:
-            sex_ = 0
+            sex = 0
         self.vk_url = 'https://api.vk.com/method/'
         self.params = {
             "access_token": access_token,
             "v": 5.131,
             'oauth': 1,
-            'count': 100,
+            'count': 10,
             'offset': random.randrange(0, 100),
             'sort': 0,
-            "fields": 'sex, city, photo_id, screen_name, can_write_private_message',
+            "fields": 'sex, city, photo_id, has_photo, screen_name, can_write_private_message',
             "age_from": age - 3,
             "age_to": age + 3,
-            "sex": sex_,
-            "hometown": city,
+            "sex": sex,
+            "hometown": city.title(),
             "status": 1,
             "album_id": "profile"
         }
 
     """Получаем информацию для запроса урла фото"""
 
-    def _get_all_result(self):
+
+    def get_all_result(self):
         url_get_info = self.vk_url + "users.search?"
         req = requests.get(url_get_info, params=self.params).json()
         all_result_list = [req['response']['items']]
         return all_result_list
 
-    def get_inf(self):
+
+    def get_inf(self, user_id):
         try:
-            all_result_list = self._get_all_result()
+            all_result_list = self.get_all_result()
             for items in all_result_list:
                 for i in range(len(items)):
                     item = items[i]
-                    if item['is_closed'] is False and item['can_write_private_message'] == 1:
-                        print(item)
-                        result = item['first_name'], item['last_name'], item[
-                            'id'], f"https://vk.com/{item['screen_name']}"
-                        return result
+                    if item['is_closed'] is False and item['can_write_private_message'] == 1 and item['has_photo'] == 1:
+                        if f"{user_id}-{item['id']}" not in coincidence:
+                            result = item['first_name'], item['last_name'], item[
+                                'id'], f"https://vk.com/{item['screen_name']}"
+                            coincidence.append(f"{user_id}-{item['id']}")
+                            print(coincidence)
+                            return result
                     else:
                         continue
         except:
             KeyError("Проверьте правильность набора!")
-        return None
+            return None
 
 
 class VKinder_get_photo:
@@ -105,7 +105,3 @@ class MessagesSend:
         url_send_message = self.vk_url + "messages.send?"
         req = requests.post(url_send_message, params=self.params).json()
         return req
-
-
-
-
